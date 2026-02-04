ae70e7861bd60919bca45514eba1aa67157c23c8
SivaAnandan <107688705+SivaAnandan@users.noreply.github.com>
2026-02-05 02:16:50 +0530

    Add files via upload

---
+++ HBSADConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using HBSADLibrary.BusinessLogic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+
+using IHost host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
+
+//var configuration = new ConfigurationBuilder()
+//                      .AddJsonFile("appsettings.json")
+//                      .Build();
+
+
+
+try
+{
+    var app = host.Services.GetRequiredService<IUserRead>();
+
+    app.UserFileReader();
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureServices((_, services) =>
+        {
+            services.AddSingleton<IUserRead, UserRead>();
+            services.AddLogging(builder=>builder.AddConsole());
+            //services.AddSingleton<HBSADConsole.App>();
+        });
+        
+}
+
+
